5b985596fc1af3d037754d4627c4dab49199c3c6
vanghoa92-wq <vanghoa92@gmail.com>
2025-12-14 14:03:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -347,4 +347,4 @@ if st.button("🚀 SOẠN GIÁO ÁN NGAY"):
 
 # --- CHÂN TRANG ---
 st.markdown("---")
-st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Vàng Thị Hoa - Trường PTDTBT Tiểu Cán Chu Phìn - ĐT: 097 1962215910</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Vàng Thị Hoa - Trường PTDTBT Tiểu Cán Chu Phìn - ĐT: 0962215910 </div>", unsafe_allow_html=True)
